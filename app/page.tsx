865b7f02a1d40e7465401e23071e742de3a420d1
cyberraff <cyberraff@gmail.com>
2024-05-08 23:03:20 +0100

    about page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-import Image from 'next/image';
+import Header from './components/Header';
+import About from './components/About';
 
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
+		<main className='flex min-h-screen flex-col items-center justify-between'>
+			<Header />
+			<About />
 			<p>KPO</p>
 		</main>
 	);
